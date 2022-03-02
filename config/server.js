e7c8c9995687d55d41519fa9d37c3cbe09e67137
Ronald Chan <ronaldchan@13-90-252-113-on-nets.com>
2022-03-02 21:26:17 +0800

    udpate server js env to static

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env('PORT', 8080),
+  port: 8080,
   app: {
     keys: ['0LMI89Sm1vA4nqJw9/sGSA==', 'z6QoCPENr5XcibznV8Ge7g==', 'DK7wOJdyTxAnY5O+BdAKKw==', '/Pc14BM94TX5CqRZv7Y8rA=='],
   },
